a3017ea05872c9a08bb39a16d53990666ac3180c
Axel <axosar@gmail.com>
2019-07-08 18:51:14 -0500

    Test CardList
    Testing CardList Component

---
+++ src/components/CardList.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import CardList from './CardList';
+
+it('expect to render the Card Component', () => {
+  const mockRobots = [
+    {
+      id: 1,
+      name: 'John Snow',
+      username: 'johnjohn',
+      email: 'john@gmail.com'
+    }
+  ]
+  expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+});
